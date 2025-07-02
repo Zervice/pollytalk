f1a7b123cc21744f38ad7a5c5cb5531339dd33e6
cc <1176262251@qq.com>
2025-07-02 15:23:21 +0800

    Add chatbot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning>
       <head>
+        <script async src="https://dev.zervice.us/ava/chatbot.js" data-config="eyJpZCI6ImNiX2RmYmFlMTM5YTZhMDRmYTc5MWQ2ZjE5ZGJmYjcwMWRlIiwidG9rZW4iOiJhY19jYTQyZjg3Njc3OGE0ZWNkYWI1OTc0OGUxMmI3NGFjMCJ9" data-id="6f7e685f-a115-4f97-9b7f-f19631d84646" data-js="https://uma.zervice.us/script.js"></script>
         <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "4bcf3f093bd540b1abf323957828f8b2"}'></script>
         <script defer src="https://cloud.umami.is/script.js" data-website-id="0224687c-8896-4c82-8b39-8356fcfc150b"></script>
       </head>
