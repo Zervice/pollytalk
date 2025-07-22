e6215e7343e894ec1e1091f5721dcce7c209c53c
heruiwoniou <heruiwoniou@qq.com>
2025-07-22 22:13:52 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning>
       <head>
-        <script async src="https://dev.zervice.us/ava/chatbot.js" data-config="eyJpZCI6ImNiX2I1OTc1ODQ3NDg3ZjQwZDE5OTU1MjZlNjlhZTU1NTdiIiwidG9rZW4iOiJhY19lODdiNmFmMzJmZDU0ZGUyOWZlZWJlMDE0YWU1MWRlZiIsIm5hbWUiOiJDaGF0Ym90In0=" data-id="e3c41a52-e3ea-4702-8f1d-30ce90a16f7b" data-js="https://uma.zervice.us/script.js"></script>
+        <script async src="https://dev.zervice.us/ava/chatbot.js" data-config="eyJpZCI6ImNiXzI0YmQ4ODE1MDRhODQxYzFhM2U4MzlmYWQzNTM5YTVhIiwidG9rZW4iOiJhY19kZDdlMDQyOTNjMGE0ZTBjYTEzZmE1OTExZWQ4ZWVhMCIsIm5hbWUiOiJDaGF0Ym90In0=" data-id="feff6376-ce05-4d74-b124-b93766b43211" data-js="https://uma.zervice.us/script.js"></script>
         <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "4bcf3f093bd540b1abf323957828f8b2"}'></script>
         <script defer src="https://cloud.umami.is/script.js" data-website-id="0224687c-8896-4c82-8b39-8356fcfc150b"></script>
       </head>
